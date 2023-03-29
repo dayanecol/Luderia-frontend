c95763ab0debe16a77e46c263539a27a0961ac83
Dayane <dayanecarolina.oliveira@gmail.com>
2023-03-28 23:40:55 -0300

    feat: create initial header component structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+import Header from "../components/Header";
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default Index;
